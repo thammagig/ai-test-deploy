ff3c8fb1d61449b5ae6d4032dbe412f5452f0eeb
thammagig <44879848+thammagig@users.noreply.github.com>
2022-09-20 15:36:48 +0700

    first edition

---
+++ app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+})
+
+st.write("Here's our first attempt at using data to create a table:")
+st.write(df)
+
+
+
+
+chart_data = pd.DataFrame(
+        np.random.randn(20, 3),
+        columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+
+map_data = pd.DataFrame(
+        np.random.randn(1000, 2) / [50, 50] + [19.027695761743225, 99.89943603581405], 
+        columns=['lat', 'lon'])
+
+st.map(map_data)
